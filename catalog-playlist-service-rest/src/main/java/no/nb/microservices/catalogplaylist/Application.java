3eeef4161e94c2253c7c4559d136fb6ae5fb5463
Jim Arthur Nilsen <jim.nilsen@nb.no>
2015-10-09 13:32:08 +0200

    BT-3368

---
+++ catalog-playlist-service-rest/src/main/java/no/nb/microservices/catalogplaylist/Application.java
@@ -0,0 +1,26 @@
+package no.nb.microservices.catalogplaylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableOAuth2Resource
+@RefreshScope
+@EnableHypermediaSupport(type= {EnableHypermediaSupport.HypermediaType.HAL})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
